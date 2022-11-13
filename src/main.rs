757bd223fbde33befc985511f013ac46c6d820a0
SuperMohit <mohittalniya@gmail.com>
2022-11-13 17:12:50 -0500

    workers for processing tasks

--- src/main.rs
+++ src/main.rs
@@ -5,18 +5,29 @@ pub mod storage;
 mod job;
 mod worker;
 
+mod diffv;
+
+use diffv::model::Diff;
+use worker::workers::Worker;
+
 
 #[tokio::main]
 async fn main(){
-    let result = storage::create_client("".to_string()).await;
+    let src_client = storage::create_client("".to_string()).await;
+    let tgt_client = storage::create_client("".to_string()).await;
 
-    let m_client = match result {
+    let s_client = match src_client {
+        Ok(client) => client,
+        Err(error) => panic!("error creating mongo client: {:?}", error),
+    };
+
+    let t_client = match tgt_client {
         Ok(client) => client,
         Err(error) => panic!("error creating mongo client: {:?}", error),
     };
 
     let jb = JobCreator{
-        client: m_client,
+        client: s_client.clone(),
     };
 
     let job_result = jb.create_jobs().await;
@@ -25,6 +36,21 @@ async fn main(){
         Ok(res) => println!("job done {}", res),
         Err(error) => panic!("error creating jobs: {:?}", error),
     }
+
+    let d = Diff {
+        tgt_client: t_client.clone(),
+        src_client: s_client,
+        meta_client: t_client,
+        block_size: 250,
+        filters: None,
+    }; 
+    
+    let w = Worker{
+        diff: d
+    };
+
+    w.task();
+
 }
 
 
